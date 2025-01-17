05bc1433169a05a5004018da66cc0211617e4e28
DaintyDust <144209886+DaintyDust@users.noreply.github.com>
2025-01-17 08:57:57 +0100

    fixed add btn

--- Website Fluitende Fietser/js/main.js
+++ Website Fluitende Fietser/js/main.js
@@ -95,7 +95,7 @@ function add() {
     const newbtn = document.createElement('button');
     newbtn.classList.add("shop-button");
     const newimg = document.createElement('img');
-    newimg.src = "../images/fietsen/Stella_Allegra_voorwielmotor.jpg";
+    newimg.src = "../images/fietsen/Allegra voorwielmotor.jpg";
     const newlabel = document.createElement('label');
     newlabel.innerHTML = "Stella Allegra voorwielmoter";
     
