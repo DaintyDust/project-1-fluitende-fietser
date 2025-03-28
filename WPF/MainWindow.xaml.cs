ec84fccaa2599e09a12683220edf2c2c63b774ea
DaintyDust <144209886+DaintyDust@users.noreply.github.com>
2025-03-28 10:59:58 +0100

    wpf updates

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,49 @@ namespace WPF
         public MainWindow()
         {
             InitializeComponent();
+            LoadComboBoxes();
+        }
+
+        private void LoadComboBoxes()
+        {
+            string path = "Assets/prijzen_fietsen_verzekeringen_services.csv";
+            StreamReader reader = null;
+
+            if (File.Exists(path))
+            {
+                reader = new StreamReader(File.OpenRead(path));
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine().Replace("\"", "");
+                    var values = line.Split(';');
+
+                    if (values[0] == "Fietsen")
+                    {
+                        FietsenCombobox.Items.Add($"{values[1]}\n{values[2]}");
+                    }
+                    else if (values[0] == "Verzekeringen")
+                    {
+                        VerzekeringComboBox.Items.Add($"{values[1]}\n{values[2]}");
+                    }
+                    else if (values[0] == "Services")
+                    {
+                        ServicesComboBox.Items.Add($"{values[1]}\n{values[2]}");
+
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
