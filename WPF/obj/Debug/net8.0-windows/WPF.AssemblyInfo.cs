727eff1cd96ebaad2c2e1eccd397c58f2e149a80
Niels-debug-png <PS263592@edu.summacollege.nl>
2025-04-01 11:01:45 +0200

    kaas

--- WPF/obj/Debug/net8.0-windows/WPF.AssemblyInfo.cs
+++ WPF/obj/Debug/net8.0-windows/WPF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6b897facea6f3c4b104d1727ba849d656cc354")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e437d0ed9baaea831d2e8ea6de20562f912aa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
